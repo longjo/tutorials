98396e9b2be228767e1341739f56a12c5030524d
Joseph Long <sushidot@yahoo.com>
2016-03-26 18:06:18 -0700

    git ignore fixed again
    fixed up

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mcs
+//
+//  Created by josephLong on 3/22/16.
+//  Copyright (c) 2016 josephLong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    // insert code here......
+    //insert here
+    printf("Hello, World!\n");
+    //Hey whats up...
+    return 0;
+}
